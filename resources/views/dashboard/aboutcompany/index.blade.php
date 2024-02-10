ad352198514481e922e1cff821fda79649308308
noviamarsheila <noviasheila123@gmail.com>
2024-02-10 21:14:28 +0700

    modal

--- resources/views/dashboard/aboutcompany/index.blade.php
+++ resources/views/dashboard/aboutcompany/index.blade.php
@@ -24,7 +24,7 @@
                 <td>{{ $profile->email }}</td>
                 <td>{{ $profile->workshop }}</td>
                 <td>
-                    <a href="" class="badge bg-info mb-1" data-bs-toggle="modal" onclick="showProfile({{ $profile }}) data-bs-target="#ModalShowProfile">
+                    <a href="" class="badge bg-info mb-1" data-bs-toggle="modal" onclick="showProfile({{ $profile }})" data-bs-target="#ModalShowProfile">
                         <i class="bi bi-eye" style="font-size: 1rem"></i>
                     </a>
                     <a href="./edit-aboutcompany.html" class="badge bg-warning mb-1">
@@ -46,34 +46,28 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title fw-bold text-primary" id="exampleModalLabel" id="showName">Nama Perusahaan</h5>
+                <h5 class="modal-title fw-bold text-primary" id="showName">Nama Perusahaan</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                    <h6>Deskripsi:
-                        <small id="showDeskripsi"></small>
-                    </h6>
-                    <h6>Akta Pendiri:
-                        <small id="showAkta"></small>
-                    </h6>
-                    <h6>Alamat:
-                        <small id="showAlamat"></small>
-                    </h6>
-                    <h6>Workshop:
-                        <small id="showWorkshop"></small>
-                    </h6>
-                    <h6>Email:
-                        <small id="showEmail"></small>
-                    </h6>
-                    <h6>No HP:
-                        <small id="showNohp"></small>
-                    </h6>
-                    <h6>Web:
-                        <small id="showWeb"></small>
-                    </h6>
-                    <h6>Link Map:
-                        <small id="linkMap"></small>
-                    </h6>
+                <div>
+                    <h6>Deskripsi:</h6>
+                    <p id="showDeskripsi"></p>
+                    <h6>Akta Pendiri:</h6>
+                    <p id="showAktaPendiri"></p>
+                    <h6>Alamat:</h6>
+                    <p id="showAlamat"></p>
+                    <h6>Workshop:</h6>
+                    <p id="showWorkshop"></p>
+                    <h6>Email:</h6>
+                    <p id="showEmail"></p>
+                    <h6>No HP:</h6>
+                    <p id="showNohp"></p>
+                    <h6>Web:</h6>
+                    <p id="showWeb"></p>
+                    <h6>Link Map:</h6>
+                    <p id="linkMap"></p>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -86,23 +80,23 @@
     function showProfile(profile) {
         const showName = document.getElementById('showName');
         const showDeskripsi = document.getElementById('showDeskripsi');
-        const showAktaPendiri = document.getElementById('showAkta');
         const showAlamat = document.getElementById('showAlamat');
         const showWorkshop = document.getElementById('showWorkshop');
         const showNohp = document.getElementById('showNohp');
         const showWeb = document.getElementById('showWeb');
         const showEmail = document.getElementById('showEmail');
         const showLinkMap = document.getElementById('linkMap');
+        const showAktaPendiri = document.getElementById('showAkta');
 
         showName.textContent = profile.name;
         showDeskripsi.innerHTML = profile.deskripsi;
-        showAktaPendiri.innerHTML = profile.akta_pendiri;
         showAlamat.innerHTML = profile.alamat;
         showWorkshop.innerHTML = profile.workshop;
         showNohp.innerHTML = profile.no_hp;
         showWeb.innerHTML =profile.web;
         showEmail.innerHTML = profile.email;
         showLinkMap.innerHTML = profile.link_map;
+        showAktaPendiri.innerHTML = profile.akta_pendiri ;
     }
 </script>
 
